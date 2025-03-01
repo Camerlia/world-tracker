224f3583be974166438ab88f3f96e36643b16bac
Camelia <Cameliaafolabi@gmail.com>
2025-03-01 01:38:58 +0100

    added react router dom and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Notfound from './pages/Notfound'
 
 const App = () => {
   return (
-    <div className={`text-black`}>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
